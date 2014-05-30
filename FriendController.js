bcf903e907f99b13ed39434c7903758951aef142
olivekelpie <fumikom@gmail.com>
2014-05-30 13:13:05 -0600

    first push

--- FriendController.js
+++ FriendController.js
@@ -1,5 +1,32 @@
+var app = angular.module('friendApp');
 
+app.controller('allFriends', function($scope, friendData){
 
+    friendData.myFriends
+        .then(function(data){
+            console.log('the raw data is', data);
+            $scope.people = data.data.results;
+            
+            for(var i = 0; i < data.data.results.length; i++){
+                
+                for (var key in Object) {
+                        var prop = friend_count in data.data.results;
+                        var obj = Object.friend_count;
+                         if(obj != null){
+                          obj = filterInt(obj);
+                         }
+                }
+               
+            }
+            
+             console.log('the new data is', data);
+              return data.data.results;
+         });
 
+    $scope.searchFriend = '';
+    $scope.searchLocation = '';
 
+    $scope.searchField = '';
+
+}); 
 
